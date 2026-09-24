2a50192c45aa0a03fd576c1937a785f079836371
AverageOatmeal424 <77759949+AverageOatmeal424@users.noreply.github.com>
2026-09-23 21:55:14 -0400

    Update MerchModel properties to string type
    Changed ItemPrice and ItemQuantity types from decimal and int to string.

--- Models/MerchModel.cs
+++ Models/MerchModel.cs
@@ -6,20 +6,13 @@ namespace Storefront.Models;
 public class MerchModel
 {
     [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Item_ID { get; set; }
 
-    [Required]
-    [StringLength(200)]
     public string ItemName { get; set; } = string.Empty;
 
-    [StringLength(1000)]
     public string ItemDesc { get; set; } = string.Empty;
 
-    [Range(typeof(decimal), "0", "999999999")]
-    [Column(TypeName = "decimal(10,2)")]
-    public decimal ItemPrice { get; set; }
+    public string ItemPrice { get; set; }
 
-    [Range(0, int.MaxValue)]
-    public int ItemQuantity { get; set; }
+    public string ItemQuantity { get; set; }
 }
